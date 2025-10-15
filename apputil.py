0e1dd0c1d5849369fff0a24db4119e61df0bc87c
Serena Hawkins <sjhawkins@ap.org>
2025-10-14 23:16:13 -0400

    Finishing touches on functions

--- apputil.py
+++ apputil.py
@@ -20,22 +20,25 @@ def get_pred_data(team,
                   win, 
                   teams):
     #check if the opponent is ranked 
-    opp = teams[pd.Series(teams["Team"]).str.contains(selected_opponent).tolist()]
+    #Issue with str contains not accepting argument 
+    opp = teams[pd.Series(teams["Team"]).str.startswith(selected_opponent).tolist()]
     opp_rank = opp["AP/CFP"].iloc[-1] != "--"
-    pred_data = teams[(teams["Team"] == team) & (teams["opponent"] == selected_opponent)]
+    
+    pred_data = teams[(teams["Team"] == team)& (teams["opponent"] == selected_opponent)]
 
     pred_data["points_allowed"] = selected_points_allowed
     pred_data["points_scored"] = selected_points_scored
-    pred_data["point_differential"] = selected_points_scored - selected_points_allowed
-    pred_data["OT"] = 0 
-    pred_data["OT_num"] = 0 
-    pred_data["win_loss"] = win
-    if(pred_data["home_game"]):
-        pred_data["home_game"] = 1
+    pred_data.loc[:,"point_differential"] = pred_data["points_scored"] -  pred_data["points_allowed"]
+    pred_data.loc[:,"OT"] = 0 
+    pred_data.loc[:,"OT_num"] = 0 
+    #print(win)
+    pred_data.loc[:,"win_loss"] = win[0]
+    if(win[0] == "W"):
+            pred_data.loc[:,"win_loss"] = 1
     else: 
-        pred_data["home_game"] == 0 
+            pred_data.loc["", "win_loss"] == 0 
     pred_data["ranked_opponent"] = opp_rank
-    final_pred_data = pred_data["win_loss", "ranked_opponent", "point_differential"]
+    final_pred_data = pred_data[["win_loss", "ranked_opponent", "point_differential"]]
     return(final_pred_data)
 
 def get_pred(data, model):
@@ -47,8 +50,14 @@ def get_results_text(model,
                   selected_opponent, 
                   selected_points_scored, 
                   selected_points_allowed, 
-                  win): 
-    data = get_pred_data()
+                  win, 
+                  teams): 
+    data = get_pred_data(team, 
+                  selected_opponent, 
+                  selected_points_scored, 
+                  selected_points_allowed, 
+                  win, 
+                  teams)
     rank_change = get_pred(data, model)
     if(win == "W"): 
         text_result = "beat"
@@ -59,11 +68,5 @@ def get_results_text(model,
         direction = "move down by"
     else: 
         direction = "move up by"         
-    return_text = f"If the {team} \
-        {text_result} \
-            {selected_opponent} by \
-                {abs(selected_points_scored - selected_points_allowed)}\
-                    points, they will {direction}\
-                        {abs(rank_change)}\
-                            ranking points"    
+    return_text = f"If the {team} {text_result} {selected_opponent} by {abs(selected_points_scored - selected_points_allowed)} points, they will {direction} {abs(rank_change[0])} ranking points"    
     return(return_text)
